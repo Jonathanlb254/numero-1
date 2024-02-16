05e6c0607cf03140b2e24b8c66861d0b00f34b52
Jonathanlb254 <159447643+Jonathanlb254@users.noreply.github.com>
2024-02-15 22:45:18 -0500

    Update potencia.java

--- potencia.java
+++ potencia.java
@@ -1,18 +1,22 @@
-public class PotenciaRecursiva {
-
+public class PotenciaSumaRecursiva {
     public static void main(String[] args) {
         int base = 2;
         int exponente = 3;
-        int resultado = calcularPotencia(base, exponente);
+        int resultado = calcularPotenciaSuma(base, exponente);
         System.out.println(base + " elevado a la " + exponente + " es igual a: " + resultado);
     }
 
     // Función recursiva para calcular la potencia usando solo la suma
-    public static int calcularPotencia(int base, int exponente) {
+    public static int calcularPotenciaSuma(int base, int exponente) {
         if (exponente == 0) {
             return 1;
         } else {
-            return base * calcularPotencia(base, exponente - 1);
+            return suma(base, calcularPotenciaSuma(base, exponente - 1));
         }
+    }
+
+    // Función para sumar dos números
+    public static int suma(int a, int b) {
+        return a + b;
     }
 }
